3ffb5ed0165074abbf7821571498e9d4054c9c7f
Carlos Flores <cflor083@fiu.edu>
2017-10-26 14:41:43 +0000

    Implemented the POST method such that it stores the incoming JSON packet.

--- sensor_server.py
+++ sensor_server.py
@@ -1,4 +1,6 @@
+from flask import request
 from flask_api import FlaskAPI
+from datetime import datetime
 import json
 
 # Rest API
@@ -11,18 +13,28 @@ def load_data():
     f.close()
     return data
 
+# Used to save JSON to output file (later to be saved to DB).
+def store_data(sensor_data):
+    f = open('test.json', 'w')
+    f.write(json.dumps(sensor_data, indent=2))
+    f.close()
+
 # Used to send data to Android app.
 @app.route('/getsensorvalues', methods=['GET'])
 def get_data():
     return load_data()
 
 # Used to grab data from the LPC1768 and output to file (later to DB).
-@app.route('/sendsensorvalue', methods=['PUT'])
+@app.route('/sendsensorvalue', methods=['POST'])
 def put_data():
-    print("This will eventually be filled by something.")
+    sensor_data = request.data # Grabbing JSON Dict sent from POST Request.
+    store_data(sensor_data)
+    print "++++++++++++++++++++++++++++++++\n"
+    print "Saving Into \"test.json\" ..."
+    print "\n++++++++++++++++++++++++++++++++"
+    return "Packet was recieved and stored.\n" 
 
 try:
-    print(load_data())
     app.run(host="0.0.0.0") # Setting host to default route to allow LAN access.
 except KeyboardInterrupt:
     print("Exiting web service...")
